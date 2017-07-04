8d63fb6a6972fb11464627d1fe76a1557b861531
Tharaka Ranatunga <tharaka.14@cse.mrt.ac.lk>
2017-07-04 17:07:16 +0530

    seperating controllers from routes

---
+++ controllers/mainController.js
@@ -0,0 +1,9 @@
+/**
+ * Created by tharaka_ra on 7/4/2017.
+ */
+module.exports= {
+    getView: function (req, res) {
+        res.render('', { title: 'Express' });
+
+    }
+};
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,8 @@
 var express = require('express');
 var router = express.Router();
-
+var index = require('../controllers/mainController');
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+
+router.get('/',index.getView);
 
 module.exports = router;
