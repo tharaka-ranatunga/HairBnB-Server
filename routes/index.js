d30b1788cf4494d6ed282b4f511df798c75e7f20
Tharaka Ranatunga <Tharaka Ranatunga>
2017-07-04 22:59:06 +0530

    installing mysql

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var index = require('../controllers/mainController');
 /* GET home page. */
 
 router.get('/',index.getView);
-router.get('/test',index.viewJson);
+router.post('/test',index.viewJson);
 
 
 module.exports = router;
