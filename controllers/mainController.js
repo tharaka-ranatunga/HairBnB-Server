bca8f80788312974316d98a00fd8b82ce4b6331b
Tharaka Ranatunga <tharaka.14@cse.mrt.ac.lk>
2017-07-06 11:08:06 +0530

    minor changes

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -2,6 +2,19 @@
  * Created by tharaka_ra on 7/4/2017.
  */
 var user = require('../models/user');
+// var values = [
+//     {firstName:'demian', lastName: 'demian@gmail.com', email: 'demian@gmail.com', password: 1},
+//     {firstName:'john'  , lastName: 'demian@gmail.com', email: 'john@gmail.com'  , password: 2},
+//     {firstName:'mark'  , lastName: 'demian@gmail.com', email: 'mark@gmail.com'  , password: 3},
+//     {firstName:'pete ' , lastName: 'demian@gmail.com', email: 'pete@gmail.com'  , password: 4}
+// ];
+
+var post  = {
+    firstname:'Demian',
+    lastname:'Martin',
+    email:'demianmartin@gmail.com',
+    password:'encrypted'
+};
 
 module.exports= {
     getView: function (req, res) {
@@ -14,8 +27,8 @@ module.exports= {
     },
     insert: function(req,res){
         user.insertUser(function (name) {
-            return res.json({name:name});
-        });
+            return res.json({message:name});
+        },post);
 
     },
 
--- models/user.js
+++ models/user.js
@@ -3,11 +3,11 @@
  */
 var db = require('../dbconnect');
 
-exports.insertUser = function (callback) {
+exports.insertUser = function (callback,post) {
     // var post  = {firstname:'me', lastname:'you', email:'hi', password:'yes'};
-    // db.query('INSERT INTO user (firstName, lastName , email, password) VALUES (?,?,?,?)', ['ytfyf','gfhgfg','jugf','fhgg'], function(err, rows) {
-    db.query('select * from user', function(err, rows) {
+    db.query('INSERT INTO user (firstName, lastName , email, password) VALUES (?,?,?,?)', [post.firstname,post.lastname,post.email,post.password], function(err, rows) {
+    // db.query('select * from user', function(err, rows) {
         if (err) throw err;
-        return callback(rows);
+        return callback("success");
     });
 };
