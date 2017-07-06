90754f623e62da175b6ed3e164588f02d88476e9
Tharaka Ranatunga <tharaka.14@cse.mrt.ac.lk>
2017-07-06 12:10:25 +0530

    User Registration

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -2,18 +2,15 @@
  * Created by tharaka_ra on 7/4/2017.
  */
 var user = require('../models/user');
-// var values = [
-//     {firstName:'demian', lastName: 'demian@gmail.com', email: 'demian@gmail.com', password: 1},
-//     {firstName:'john'  , lastName: 'demian@gmail.com', email: 'john@gmail.com'  , password: 2},
-//     {firstName:'mark'  , lastName: 'demian@gmail.com', email: 'mark@gmail.com'  , password: 3},
-//     {firstName:'pete ' , lastName: 'demian@gmail.com', email: 'pete@gmail.com'  , password: 4}
-// ];
+var passwordHash = require('password-hash')
+
+var password = passwordHash.generate('encrypted')
 
 var post  = {
     firstname:'Demian',
     lastname:'Martin',
     email:'demianmartin@gmail.com',
-    password:'encrypted'
+    password:password
 };
 
 module.exports= {
