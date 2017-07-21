51c641bbcf1a9f217a08ddf8da31cb64d3f68550
Tharaka Ranatunga <tharaka.14@cse.mrt.ac.lk>
2017-07-21 13:58:44 +0530

    profile creation done

--- seeders/20170713091944-role.js
+++ seeders/20170713091944-role.js
@@ -26,12 +26,5 @@ module.exports = {
   },
 
   down: function (queryInterface, Sequelize) {
-    /*
-      Add reverting commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.bulkDelete('Person', null, {});
-    */
   }
 };
--- seeders/20170720061508-skill_type.js
+++ seeders/20170720061508-skill_type.js
@@ -4,16 +4,20 @@ module.exports = {
   up: function (queryInterface, Sequelize) {
       return queryInterface.bulkInsert('skill_types',[
           {skill_name: 'Barber'},
-          {skill_name: 'Makeup'},
-          {skill_name: 'Dry cutting'},
+          {skill_name: 'Freehand'},
+          {skill_name: 'Razor'},
           {skill_name: 'Shaving'},
-          {skill_name: 'Hair styling'},
-          {skill_name: 'Wigs cutting'},
-          {skill_name: 'Curling'},
-          {skill_name: 'Coloring'},
-          {skill_name: 'Color correction'},
+          {skill_name: 'Scissor over comb'},
+          {skill_name: 'Texturing'},
+          {skill_name: 'Clipper work'},
+          {skill_name: 'Classics'},
+          {skill_name: 'Dry cutting'},
+          {skill_name: 'Wig cutting'},
           {skill_name: 'Long hair'},
-          {skill_name: 'Short hair'}
+          {skill_name: 'Short hair'},
+          {skill_name: 'Makeup'},
+          {skill_name: 'Hair coloring'}
+
       ])
   },
 
